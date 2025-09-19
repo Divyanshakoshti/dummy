cedfeabc29942db1c9815567de06ddabea2cfb3c
Divyanshakoshti <divyanshakoshti@gmail.com>
2025-09-19 22:49:45 +0530

    Add files via upload

---
+++ lect16/array4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int BSearch(){
+    int arr[6]={1,2,3,4,5,6};
+    int target=5;
+    int s=0;
+    int end=5;
+    while(s<=end){
+        int mid=(s+end)/2;
+        if(arr[mid]==target){
+            return mid;
+        }
+        else if(arr[mid]<target){
+            s=mid+1;
+        }else
+            end=mid-1;
+           
+    }
+    return -1;
+}
+int main(){
+    int data=BSearch();
+    printf("%d",data);
+}
