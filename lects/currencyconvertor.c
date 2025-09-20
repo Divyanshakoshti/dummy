9dcee97cf9b9611b3cc848814508f3fa114cf950
Divyanshakoshti <divyanshakoshti@gmail.com>
2025-09-20 10:40:46 +0530

    basic

---
+++ lects/currencyconvertor.c
@@ -0,0 +1,10 @@
+/*#include<stdio.h>
+int main(){
+    int a;
+    printf("enter amount in indian");
+    scanf("%d",&a);
+    char ch;
+    printf("enter what you want to convert");
+    scanf("%c",&ch);
+
+}*/
