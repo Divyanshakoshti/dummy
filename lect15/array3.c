208719b0f5417af5b63375e021f00b4c2500a293
Divyanshakoshti <divyanshakoshti@gmail.com>
2025-09-20 09:36:03 +0530

    lect15

---
+++ lect15/array3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int arr[5]={-8,2,-3,4,9};
+    int maxSum=0;
+    for(int i=0;i<5;i++){
+        int currSum=0;
+        for(int j=i;j<5;j++){
+            currSum=currSum+arr[j];
+            if(currSum>maxSum){
+                maxSum=currSum;
+            }
+        }
+    }
+    printf("%d",maxSum);
+}
