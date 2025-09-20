b793a04e17eaf26e30a23481046442bda0c8fb53
Divyanshakoshti <divyanshakoshti@gmail.com>
2025-09-20 09:43:22 +0530

    loop

---
+++ loop/for.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+int main 
+
+
+
+
+
+
+
+
+
+
+
+
+   /* int sum=0;
+    for(int i=1;i<=5;i++){
+        sum=sum+i;
+    }
+    printf("%d\n",sum);
+}*/
+     
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /* odd for(int i=10;i>0;i--){
+            if(i%2==1){
+                printf("%d\n",i);
+            }
+        }
+    }  */        
+          
+          
+          
+            /* even if(i%2==0){
+                printf("%d\n",i);
+            }
+        }
+        
+        }*/
+
+
+
+      
+    /* for(int i=10;i>0;i--){
+        printf("%d\n",i);
+    }*/
