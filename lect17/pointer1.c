6341f6e6a48f15e260357190d3b18f674e082eec
Divyansha <divyanshakoshti@gmail.com>
2025-10-26 01:54:57 +0530

    pointer

---
+++ lect17/pointer1.c
@@ -0,0 +1,15 @@
+ #include <stdio.h>
+ int sum(int *a,int *b){
+     *a=20;
+     *b=10;
+     int sum=(*a)+(*b);
+     return sum;
+
+ }
+ int main(){
+     int a=5;
+     int b=10;
+    int data=sum(&a,&b);
+   printf("%d\n",data);
+   printf("%d",a+b);
+ }
